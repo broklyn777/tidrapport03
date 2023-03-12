8dc83c002ffa6d21ce69ecb8309f00ddc885128c
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2023-03-12 15:35:33 +0100

    börjar m filter fungerar

--- pages/tid-okey1.js
+++ pages/tid-okey1.js
@@ -10,21 +10,10 @@ import { useState, useEffect } from 'react'
 
 
 export default function Tidrapport({ reports }) {
-    const [searchTerm, setSearchTerm] = useState('');
 
-    function handleSearch(event) {
-        setSearchTerm(event.target.value);
-    }
 
     const [date, setDate] = useState(new Date());
     const [filteredReports, setFilteredReports] = useState(reports);
-
-    useEffect(() => {
-        const filtered = reports.filter((report) =>
-            report.projectInfo.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        setFilteredReports(filtered);
-    }, [reports, searchTerm]);
 
     function handleDateChange(e) {
         setDate(new Date(e.target.value));
@@ -47,34 +36,35 @@ export default function Tidrapport({ reports }) {
             </h1>
             <div className="flex justify-end mb-4">
                 <input
-                    type="text"
-                    placeholder="Sök efter projekt..."
-                    value={searchTerm}
-                    onChange={handleSearch}
+                    type="date"
+                    value={format(date, "yyyy-MM-dd")}
+                    onChange={handleDateChange}
                     className="border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 block w-48 sm:text-sm border rounded-md"
                 />
                 <button
-                    className="ml-2 px-3 py-1 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md"
-                    onClick={() => setSearchTerm('')}
+
+                    className="ml-4 px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                    Rensa
+                    Visa
                 </button>
             </div>
-
-            <ul>
+            <ul className="divide-y divide-gray-200">
                 {filteredReports.length > 0 ? (
                     filteredReports.map((report) => (
-                        <li key={report.id}>
-                            <p>Användarnamn: {report.username}</p>
-                            <p>Projektinformation: {report.projectInfo}</p>
-                            <p>Datum: {format(new Date(report.date), "dd MMMM yyyy", { locale: sv })}</p>
-                            <p>Tid in: {report.timeIn}</p>
-                            <p>Tid ut: {report.timeOut}</p>
-                            <p>Antal arbetade timmar: {report.hours}</p>
+                        <li key={report.id} className="py-4">
+                            <div className="flex justify-between">
+                                <h2 className="text-lg font-medium">{report.projectInfo}</h2>
+                                <p className="text-gray-500 text-sm">{report.username}</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p className="text-gray-500 text-sm">{format(new Date(report.date), "dd MMMM yyyy", { locale: sv })}</p>
+                                <p className="text-gray-500 text-sm">{report.timeIn} - {report.timeOut}</p>
+                            </div>
+                            <p className="text-gray-700">{report.description}</p>
                         </li>
                     ))
                 ) : (
-                    <p>Inga tidrapporter för valt projekt.</p>
+                    <p>Inga tidrapporter för valt datum.</p>
                 )}
             </ul>
 
